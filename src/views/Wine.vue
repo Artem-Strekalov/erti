4d8190ea26a1616e7022dfcf2ea7908d73065a60
Artem-Strekalov <48091807+Artem-Strekalov@users.noreply.github.com>
2021-01-25 16:00:46 +0300

    added-menu wine

--- src/views/Wine.vue
+++ src/views/Wine.vue
@@ -4,7 +4,7 @@
       <div class="navigation">
         <a><router-link :to="{ name: 'MainPage' }">ГЛАВНАЯ</router-link></a>
         <a><router-link :to="{ name: 'Menu' }">МЕНЮ</router-link></a>
-        <a><router-link :to="{ name: 'Menu' }">ВИНО</router-link></a>
+        <a><router-link :to="{ name: 'Wine' }">ВИНО</router-link></a>
         <a><router-link :to="{ name: 'Сoncept' }">КОНЦЕПЦИЯ</router-link></a>
         <img src="../assets/erti-logo.png" alt="" />
         <a><router-link :to="{ name: 'Foto' }">МЕДИА </router-link></a>
@@ -13,16 +13,105 @@
         <a><router-link :to="{ name: 'Contacts' }">КОНТАКТЫ</router-link></a>
       </div>
       <div class="content">
-        <div class="info">
+        <div class="block"></div>
+        <div class="carusel">
+          <div class="slider js-slider">
+            <div
+              class="slider__body"
+              v-bind:style="{ left: sliderOffsetLeft + 'px' }"
+            >
+              <div
+                class="slider__slide js-slide"
+                v-for="slide in sliderList"
+                v-bind:key="slide.img"
+                :style="'background-image: url(' + slide.img + ')'"
+              >
+                <button v-on:click="prevSlide">
+                  <i class="fa fa-chevron-left" aria-hidden="true"></i></button
+                ><button v-on:click="nextSlide">
+                  <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
+          </div>
           <span>Нельзя купить счастье, но можно купить вино...</span>
         </div>
-        <div class="slider"></div>
       </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // Всего слайдов
+      sliderAllCount: 0,
+      // Номер активного слайда
+      sliderActive: 1,
+      // Отступ тела со слайдами в контейнере
+      sliderOffsetLeft: 0,
+      // Шаг одного слайда = его длина
+      sliderOffsetStep: 0,
+      // Список изображений
+      sliderList: [
+        {
+          img: "https://i.ibb.co/rHcWX9D/wine1.png",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    // Иницилизация слайдера
+    initSlider: function () {
+      // Получаем элементы сладера и его слайдов
+      let sliderBody = this.$el.querySelector(".js-slider");
+      let sliderSlidies = sliderBody.querySelectorAll(".js-slide");
+      // Записываем длину одного слайда для перелистывания
+      this.sliderOffsetStep = sliderBody.clientWidth;
+      // Общее количество слайдов для стопов
+      this.sliderAllCount = sliderSlidies.length;
+    },
+
+    // Открыть слайд по номеру
+    openSlide: function (id) {
+      if (id > 0 && id <= this.sliderAllCount) {
+        this.sliderActive = id;
+        // Сдвигаем элемент со слайдами
+        this.sliderOffsetLeft = -(
+          this.sliderActive * this.sliderOffsetStep -
+          this.sliderOffsetStep
+        );
+      }
+    },
+
+    // Следующий слайд
+    nextSlide: function () {
+      if (this.sliderActive < this.sliderAllCount) {
+        this.sliderActive += 1;
+        this.openSlide(this.sliderActive);
+      }
+    },
+
+    // Предыдущий слайд
+    prevSlide: function () {
+      if (this.sliderActive > 1) {
+        this.sliderActive -= 1;
+        this.openSlide(this.sliderActive);
+      }
+    },
+  },
+
+  mounted() {
+    this.initSlider();
+
+    // Перенастройка слайдера при ресайзе окна
+    window.addEventListener("resize", () => {
+      this.initSlider();
+      this.openSlide(this.sliderActive);
+    });
+  },
+};
 </script>
 <style lang="scss" scoped>
 .wrapper {
@@ -38,7 +127,6 @@ export default {};
     height: 100vw;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
     @media (max-width: 1325px) {
       width: 1200px;
     }
@@ -71,26 +159,50 @@ export default {};
       display: flex;
       justify-content: space-between;
       width: 100%;
-      margin: 0 0 50px 0;
-      .info {
-        display: flex;
-        align-items: flex-end;
+      margin-bottom: 50px;
+      
+
+      .carusel {
+        width: 100%;
+        height: 500px;
+        margin-left: 20%;
+        
+        .slider {
+          width: 100%;
+          height: 100%;
+          position: relative;
+          overflow: hidden;
+          &__body {
+            min-width: auto;
+            height: 100%;
+            display: flex;
+            position: relative;
+            align-items: stretch;
+            transition: all 0.5s ease;
+          }
+          &__slide {
+            min-width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            flex: 1 100%;
+            display: flex;
+            justify-content: space-between;
+            i {
+              margin-top: 80px;
+              font-size: 50px;
+              color: white;
+              opacity: 0.3;
+            }
+          }
+        }
         span {
           font-family: Caveat;
           font-style: normal;
-          font-weight: 600;
+          font-weight: bold;
           font-size: 30px;
-          color: #ffffff;
-          margin: 0 0 200px 0;
-        }
-      }
-      .slider {
-        background: rgba(252, 252, 252, 0.25);
-        border-radius: 15px;
-        width: 56%;
-        height: 700px;
-        @media (max-height: 860px) {
-          height: 650px;
+          line-height: 18px;
+          color: white;
         }
       }
     }
