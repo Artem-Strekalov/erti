a6d36868dc2da64590778108d6972cbc0b980a25
Artem-Strekalov <48091807+Artem-Strekalov@users.noreply.github.com>
2021-01-25 19:14:02 +0300

    added slider for manu page

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -4,7 +4,7 @@
       <div class="navigation">
         <a><router-link :to="{ name: 'MainPage' }">ГЛАВНАЯ</router-link></a>
         <a><router-link :to="{ name: 'Menu' }">МЕНЮ</router-link></a>
-        <a><router-link :to="{ name: 'Menu' }">ВИНО</router-link></a>
+        <a><router-link :to="{ name: 'Wine' }">ВИНО</router-link></a>
         <a><router-link :to="{ name: 'Сoncept' }">КОНЦЕПЦИЯ</router-link></a>
         <img src="../assets/erti-logo.png" alt="" />
         <a><router-link :to="{ name: 'Foto' }">МЕДИА </router-link></a>
@@ -12,23 +12,111 @@
         <a><router-link :to="{ name: 'OurTeam' }">НАША КОМАНДА</router-link></a>
         <a><router-link :to="{ name: 'Contacts' }">КОНТАКТЫ</router-link></a>
       </div>
-      <div class="content"></div>
+      <div class="content">
+        <div class="carusel">
+          <div class="slider js-slider">
+            <div
+              class="slider__body"
+              v-bind:style="{ left: sliderOffsetLeft + 'px' }"
+            >
+              <div
+                class="slider__slide js-slide"
+                v-for="slide in sliderList"
+                v-bind:key="slide.img"
+                :style="'background-image: url(' + slide.img + ')'"
+              >
+                <button v-on:click="prevSlide">
+                  <i class="fa fa-chevron-left" aria-hidden="true"></i></button
+                ><button v-on:click="nextSlide">
+                  <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // Всего слайдов
+      sliderAllCount: 0,
+      // Номер активного слайда
+      sliderActive: 1,
+      // Отступ тела со слайдами в контейнере
+      sliderOffsetLeft: 0,
+      // Шаг одного слайда = его длина
+      sliderOffsetStep: 0,
+      // Список изображений
+      sliderList: [
+        {
+          img: "https://i.ibb.co/rHcWX9D/wine1.png",
+        },
+        
+      ],
+    };
+  },
+  methods: {
+    // Иницилизация слайдера
+    initSlider: function () {
+      // Получаем элементы сладера и его слайдов
+      let sliderBody = this.$el.querySelector(".js-slider");
+      let sliderSlidies = sliderBody.querySelectorAll(".js-slide");
+      // Записываем длину одного слайда для перелистывания
+      this.sliderOffsetStep = sliderBody.clientWidth;
+      // Общее количество слайдов для стопов
+      this.sliderAllCount = sliderSlidies.length;
+    },
+    // Открыть слайд по номеру
+    openSlide: function (id) {
+      if (id > 0 && id <= this.sliderAllCount) {
+        this.sliderActive = id;
+        // Сдвигаем элемент со слайдами
+        this.sliderOffsetLeft = -(
+          this.sliderActive * this.sliderOffsetStep -
+          this.sliderOffsetStep
+        );
+      }
+    },
+    // Следующий слайд
+    nextSlide: function () {
+      if (this.sliderActive < this.sliderAllCount) {
+        this.sliderActive += 1;
+        this.openSlide(this.sliderActive);
+      }
+    },
+    // Предыдущий слайд
+    prevSlide: function () {
+      if (this.sliderActive > 1) {
+        this.sliderActive -= 1;
+        this.openSlide(this.sliderActive);
+      }
+    },
+  },
+  mounted() {
+    this.initSlider();
+    // Перенастройка слайдера при ресайзе окна
+    window.addEventListener("resize", () => {
+      this.initSlider();
+      this.openSlide(this.sliderActive);
+    });
+  },
+};
 </script>
 <style lang="scss" scoped>
 .wrapper {
   display: flex;
   flex-direction: column;
-  background: url("../assets/menu-bg.jpg") no-repeat;
+  background: url("../assets/kinza.jpg") no-repeat;
   background-size: cover;
   background-position: center;
   overflow: hidden;
   height: 100vh;
   .container {
+  
     width: 1280px;
     @media (max-width: 1325px) {
       width: 1200px;
@@ -41,7 +129,7 @@ export default {};
       flex-direction: row;
       justify-content: space-between;
       align-items: center;
-      margin: 0 0 100px 0;
+
       position: relative;
       height: 154px;
       width: 100%;
@@ -60,7 +148,42 @@ export default {};
       }
     }
     .content {
+  width: 1000px;
+      .carusel {
+        width: 100%;
+        height: 500px;
+        
+        
+        .slider {
+          width: 100%;
+          height: 100%;
+          position: relative;
+          overflow: hidden;
+          &__body {
+            min-width: auto;
+            height: 100%;
+            display: flex;
+            position: relative;
+            align-items: stretch;
+            transition: all 0.5s ease;
+          }
+          &__slide {
+            min-width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            flex: 1 100%;
+            display: flex;
+            justify-content: space-between;
+            i {
+              margin-top: 80px;
+              font-size: 50px;
+              color: white;
+              opacity: 0.3;
+            }
+          }
+        }
     }
   }
-}
+}}
 </style>
