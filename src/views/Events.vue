b54a1d5c40b577f430719ca69211d3d3a191fb45
Artem-Strekalov <48091807+Artem-Strekalov@users.noreply.github.com>
2021-01-30 13:37:18 +0300

    correction-slider

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,18 +1,75 @@
 <template>
-  <div>
-   
+  <div class="main">
+    <div class="nav"></div>
+
+    <div class="content" data-app>
+      <v-app>
+        <div>
+          <v-carousel v-model="model"  show-arrows=false>
+            <v-carousel-item
+              
+              v-for="(item, i) in items"
+              :key="i"
+              :src="item.src"
+            ></v-carousel-item>
+          </v-carousel>
+        </div>
+      </v-app>
+    </div>
   </div>
-  
+  <!-- cycle
+        height=100%
+        hide-delimiter-background
+        show-arrows-on-hover -->
 </template>
 <script>
 export default {
   data() {
     return {
-      dialog: false,
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+      model: 0,
     };
+  },
+  methods: {
+    slider() {
+      if (this.model == 1) {
+        console.log("сейчас");
+      }
+    },
+  },
+  beforeUpdate() {
+    this.slider();
   },
 };
 </script>
 <style lang="scss" scoped >
-
+.main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .nav {
+    width: 500px;
+    height: 30px;
+    background-color: red;
+  }
+  .content {
+    width: 600px;
+    height: 600px;
+  }
+}
 </style>
